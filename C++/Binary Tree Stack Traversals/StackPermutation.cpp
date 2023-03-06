7205c78b2a21dd1b683170af63038c23b1aa4bc9
Anurag Dutta <imanurag18@gmail.com>
2023-03-06 12:20:51 +0530

    Update StackPermutation.cpp

--- C++/Binary Tree Stack Traversals/StackPermutation.cpp
+++ C++/Binary Tree Stack Traversals/StackPermutation.cpp
@@ -1,14 +1,14 @@
 class Solution{
 public:
    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
-       stack<int> st;
+       stack<int> st;      //Stack is created using hashmap
        int j=0;
        for(int i=0;i<N;i++){
            while(!st.empty() and st.top() == B[j]){
-               st.pop();
+               st.pop();         //popping or removing the elements from the top of the stack
                j++;
            }
-           st.push(A[i]);
+           st.push(A[i]);        //pusing the elements of the array to the stack
        }
        while(!st.empty() and st.top() == B[j]){
            st.pop();
